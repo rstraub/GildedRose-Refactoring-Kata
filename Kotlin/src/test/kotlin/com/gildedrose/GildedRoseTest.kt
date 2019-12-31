571b50d5c0f6c607030a686fa945000dd0407cf6
Roy Straub <roy.straub@codesquad.nl>
2019-12-31 08:26:31 +0100

    test the quality deduction for regular items

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -14,6 +14,28 @@ internal class GildedRoseTest {
 
         assertEquals(9, result.sellIn)
     }
+
+    @Test
+    fun `should deduct the quality for a regular item`() {
+        val items = arrayOf(Item("test", 10, 10))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        val result = app.items.first()
+
+        assertEquals(9, result.quality)
+    }
+
+    @Test
+    fun `quality of an item can never become negative`() {
+        val items = arrayOf(Item("test", 10, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        val result = app.items.first()
+
+        assertEquals(0, result.quality)
+    }
 }
 
 
