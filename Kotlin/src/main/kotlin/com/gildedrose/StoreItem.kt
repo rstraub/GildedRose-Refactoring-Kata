9181b3df267c73188a79a1545dfaf084092c0e37
Roy Straub <roy.straub@codesquad.nl>
2020-01-03 11:38:43 +0100

    Legendary in factory

--- Kotlin/src/main/kotlin/com/gildedrose/StoreItem.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/StoreItem.kt
@@ -6,13 +6,12 @@ open class StoreItem(
     quality: Int
 ) : Item(name, sellIn, quality) {
     companion object {
-        fun fromItem(item: Item) : StoreItem {
-            return when(item) {
+        fun fromItem(item: Item) =
+            when (item) {
                 ::isAgedBrie -> AgedItem(item.name, item.sellIn, item.quality)
+                ::isLegendary -> LegendaryItem(item.name, item.sellIn, item.quality)
                 else -> StoreItem(item.name, item.sellIn, item.quality)
             }
-        }
-
 
         private fun isAgedBrie(item: Item) = item.name == GildedRose.AGED_BRIE
         private fun isLegendary(item: Item) = item.name == GildedRose.LEGENDARY
