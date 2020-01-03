57294b10a553bda6af4dc64ef4f303824159df5c
Roy Straub <roy.straub@codesquad.nl>
2020-01-03 11:37:44 +0100

    Aged brie in factory

--- Kotlin/src/main/kotlin/com/gildedrose/StoreItem.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/StoreItem.kt
@@ -7,7 +7,15 @@ open class StoreItem(
 ) : Item(name, sellIn, quality) {
     companion object {
         fun fromItem(item: Item) : StoreItem {
-            return StoreItem(item.name, item.sellIn, item.quality)
+            return when(item) {
+                ::isAgedBrie -> AgedItem(item.name, item.sellIn, item.quality)
+                else -> StoreItem(item.name, item.sellIn, item.quality)
+            }
         }
+
+
+        private fun isAgedBrie(item: Item) = item.name == GildedRose.AGED_BRIE
+        private fun isLegendary(item: Item) = item.name == GildedRose.LEGENDARY
+        private fun isBackstagePass(item: Item) = item.name == GildedRose.BACKSTAGE_PASS
     }
 }
