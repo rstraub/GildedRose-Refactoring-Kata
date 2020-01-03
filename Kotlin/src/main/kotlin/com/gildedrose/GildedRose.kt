0a6f70aab36350fec0faeaae177f5db3348250a4
Roy Straub <roy.straub@codesquad.nl>
2020-01-03 15:00:32 +0100

    Update pair to create the pair to update

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,16 +3,24 @@ package com.gildedrose
 import com.gildedrose.StoreItem.Companion.fromItem
 
 class GildedRose(var items: Array<Item>) {
+    private data class UpdatePair(val original: Item, val updated: StoreItem)
+
     fun updateInventory() = items
         .map(::fromItem)
         .map(StoreItem::withNewSellByDate)
         .map(StoreItem::withNewQuality)
-        .forEachIndexed(::updateOriginalItem)
+        .mapIndexed(::toUpdatePair)
+        .forEach(::updateOriginalItem)
 
-    private fun updateOriginalItem(index: Int, item: StoreItem) {
-        val original = items[index]
-        original.sellIn = item.sellIn
-        original.quality = item.quality
-    }
+    private fun toUpdatePair(index: Int, updated: StoreItem) =
+        UpdatePair(items[index], updated)
+
+    private fun updateOriginalItem(updatePair: UpdatePair) =
+        updatePair.original updateWith updatePair.updated
+}
+
+private infix fun Item.updateWith(item: StoreItem) {
+    sellIn = item.sellIn
+    quality = item.quality
 }
 
